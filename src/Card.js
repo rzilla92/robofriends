c3c96954ce2b7dcedee39874d146f1729dbb3d5d
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-09-07 16:01:21 +0700

    added card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='profile' src='' />
+            <div>
+                <h2>John Doe</h2>
+                <p>john.doe@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card
