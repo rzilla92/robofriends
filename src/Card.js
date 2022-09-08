af082d83b096ffb67d52f383148a8965222ae5eb
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-09-08 10:28:09 +0700

    added robot props and robohash api

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name,email,id}) => {
     return (
-        <div>
-            <img alt='profile' src='' />
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='profile' src={`https://robohash.org/${id}?200x200`} />
             <div>
-                <h2>John Doe</h2>
-                <p>john.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
