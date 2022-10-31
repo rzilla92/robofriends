96fc4563d32d71d1ce11914ee4aeaaf7d3191c34
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-10-31 10:59:49 +0700

    app.js hooks

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,50 +1,43 @@
-import React, {Component} from 'react';
+import React, { useState , useEffect} from 'react';
 import CardList from '../Components/CardList'
 import SearchBox from '../Components/Searchbox'
 import Scroll from '../Components/Scroll'
 import ErrorBoundary from '../Components/ErrorBoundary';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield:''
-        }
-    }
+function App() {
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
 
-    componentDidMount() {
+    useEffect(()=> {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({robots: users}))
-    }   
+            .then(users => setRobots(users))
+    },[]) 
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    render() {
-        const {robots,searchfield} = this.state
-        const filteredRobots = robots.filter(robots => {
-            return robots.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
-        return !robots.length ?
-            <h1>Loading</h1> :
-        (
-            <div className='tc'>
-                <div className='flex items-center justify-center'>
-                    <h1 className='f2'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                </div>
-                <Scroll>
-                    <ErrorBoundary>
-                        <CardList robots = {filteredRobots} />
-                    </ErrorBoundary>
-                </Scroll>
+    const filteredRobots = robots.filter(robots => {
+        return robots.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+    return !robots.length ?
+        <h1>Loading</h1> :
+    (
+        <div className='tc'>
+            <div className='flex items-center justify-center'>
+                <h1 className='f2'>RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange} />
             </div>
-        )
-    }
+            <Scroll>
+                <ErrorBoundary>
+                    <CardList robots = {filteredRobots} />
+                </ErrorBoundary>
+            </Scroll>
+        </div>
+    )
+    
 }
 
 export default App
